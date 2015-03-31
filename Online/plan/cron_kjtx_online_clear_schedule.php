a0bb8f7497076d97b8df32428939f6b5f088523b
cruxqixin <qqqixin@hotmail.com>
2015-03-31 14:26:26 +0800

    change cron direction

--- Online/plan/cron_kjtx_online_clear_schedule.php
+++ Online/plan/cron_kjtx_online_clear_schedule.php
@@ -14,14 +14,13 @@ $sql .= ' and status =1' ;
 $sth = $online_db->prepare ( $sql );
 $sth->execute ();
 $result_count= $sth->fetch ( PDO::FETCH_ASSOC );
-echo '共有'.$result_count['num'].'个过期的待处理预约'."\n";
+
 $sql = 'update online_schedule set status = 0 where add_time <=';
 $sql .= $now + 86400*2;
 $sql .= ' and status=1' ;
 $sth = $online_db->prepare ( $sql );
-
 $result_update= $sth->execute ();
 
-
 $end_time = microtime(true);
+echo '共有'.$result_count['num'].'个过期的待处理预约'."\n";
 echo ($end_time-$start_time) . "\n" ;exit;
